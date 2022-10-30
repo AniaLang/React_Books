63f1299649b201a8ca33d2e62c0c7a0697382cd3
Anna Lang <anna.jadwiga.lang@gmail.com>
2022-10-30 21:44:52 +0100

    clear inputs after submit, empty string  validation

--- src/Components/BookForm.js
+++ src/Components/BookForm.js
@@ -10,31 +10,33 @@ const [author, setAuthor ] = useState("")
 const [currentId, setCurrentId] = useState(0)
 
 const handleAdd = () => {
+  if(title === "" || author === ""){
+    return
+  }
   const id = currentId + 1
   const book = {title, author, id}
   setBooks([...books, book ])
   setCurrentId(id)
+  setTitle("")
+  setAuthor("")
 }
 
 const handleDelete = () => {
   setBooks(books.filter(b => !checkedBooks.includes(b)))
-
 }
-//TODO -> empty string validation
-// -> clear inputs
   return (
 <Form> 
   <Row>
     <Col>
     <Form.Group className="mb-3">
       <Form.Label>Tittle</Form.Label>
-        <Form.Control placeholder="Book Title" onChange={e => setTitle(e.target.value) } defaultValue={title}/>
+        <Form.Control placeholder="Book Title" onChange={e => setTitle(e.target.value) } value={title}/>
       </Form.Group>
     </Col>
     <Col>
       <Form.Group className="mb-3">
         <Form.Label>Author</Form.Label>
-        <Form.Control placeholder="Book Author"  onChange={e => setAuthor(e.target.value) } defaultValue={author} />
+        <Form.Control placeholder="Book Author"  onChange={e => setAuthor(e.target.value) } value={author} />
       </Form.Group>    
     </Col>
   </Row>
